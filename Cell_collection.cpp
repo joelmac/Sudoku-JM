8b48d1752c9e88e35ed70b514df2b8ca293c95f5
Joel Macaluso <joel.macaluso@gmail.com>
2017-02-19 04:12:28 +0000

    initialized memory

--- Cell_collection.cpp
+++ Cell_collection.cpp
@@ -2,10 +2,13 @@
 SudokuCell.cpp file.  Defines sudoku cell class
 */
 #include "Cell_collection.h"
+using std::vector;
+Cell_collection::Cell_collection(vector<Sudoku_cell> &Sudoku_cells)
+	:	cells( Sudoku_cells)
+{
+}
 
-Cell_collection::Cell_collection(vector<> Sudoku_cells){
-	cells=Sudoku_cells;
-	return;
+Cell_collection::~Cell_collection(){
 }
 
 void solve_single(){

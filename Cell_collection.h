4f93b4bfcd8db5b164ba61ec887f9b673beeccd1
Joel Macaluso <joel.macaluso@gmail.com>
2017-02-19 04:12:37 +0000

    initialized memory

--- Cell_collection.h
+++ Cell_collection.h
@@ -4,12 +4,13 @@
 SudokuCell.h file.  Defines struct for sudoku class
 */
 #include <vector>
+#include "Sudoku_cell.h"
 using std::vector;
 class Cell_collection {
 public:
-	Cell_collection(std::vector<>&);
+	Cell_collection(vector<Sudoku_cell>&);
 	~Cell_collection();
 	void solve_single();
-	vector<Sudoku_cell> cells[];
+	vector<Sudoku_cell> &cells;
 };
 #endif
